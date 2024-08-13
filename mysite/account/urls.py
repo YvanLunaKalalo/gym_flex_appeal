414d6be84c4ca7ee35470e95522f1ad3c3af58bb
Yvan Kalalo <kalaloyvan07@gmail.com>
2024-08-13 10:06:19 +0800

    My First Commit

---
+++ mysite/account/urls.py
@@ -0,0 +1,30 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views # Reset Password and Change Password
+
+
+urlpatterns = [
+    path('register/', views.registration_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
+    path('login/', views.login_view, name='login'),
+    path('account/', views.account_view, name='account'),
+
+    # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_change.html'), 
+    name='password_change'),
+
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), 
+    name='password_change_complete'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'),
+    name='password_reset'),
+
+    path('password_reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_done.html'),
+    name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_email.html'), 
+    name='password_reset_confirm'),
+
+    path('password_reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
+    name='password_reset_complete'),
+]
---
+++ mysite/account/views.py
@@ -0,0 +1,78 @@
+from urllib import request
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from account.forms import RegistrationForm, AccountAuthenticationForm, AccountUpdateForm
+from django.http import HttpResponse
+from django.template import loader
+
+def registration_view(request):
+	template = loader.get_template('register.html')
+	context = {}
+	if request.POST:
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			email = form.cleaned_data.get('email')
+			raw_password = form.cleaned_data.get('password1')
+			account = authenticate(email=email, password=raw_password)
+			login(request, account)
+			return redirect('login')
+		else:
+			context['registration_form'] = form
+	else: #GET request
+		form = RegistrationForm()
+		context['registration_form'] = form
+		
+	return HttpResponse(template.render(context, request))
+
+def logout_view(request):
+	logout(request)
+	return redirect('index') 
+
+def login_view(request):
+    template = loader.get_template('login.html')
+
+    context = {}
+
+    user = request.user
+
+    if user.is_authenticated:
+        return redirect('index')
+    
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+            
+            if user:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = AccountAuthenticationForm()
+
+    context['login_form'] = form
+    return HttpResponse(template.render(context, request))
+
+def account_view(request):
+	template = loader.get_template('account.html')
+
+	if not request.user.is_authenticated:
+		return redirect("login")
+
+	context = {}
+
+	if request.POST:
+		form = AccountUpdateForm(request.POST, instance=request.user)
+		if form.is_valid():
+			form.save()
+	else:
+		form = AccountUpdateForm(
+				initial= {
+					"email": request.user.email,
+					"username": request.user.username,
+				}
+			)
+	context['account_form'] = form
+	return HttpResponse(template.render(context, request))
