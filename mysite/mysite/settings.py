bfb7cbd0f7148f8288944cb2a1d18c4c0154e9f8
Yvan Kalalo <kalaloyvan07@gmail.com>
2024-10-22 11:25:16 +0800

    My Sixth Commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -136,9 +136,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_DIRS = [os.path.join(BASE_DIR, 'personal/static')]
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # for production
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
